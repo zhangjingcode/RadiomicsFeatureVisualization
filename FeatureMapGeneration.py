b67cb03984323453df774929db7fb2d53136a0aa
zhangjingcode <40156485+zhangjingcode@users.noreply.github.com>
2019-04-11 21:23:08 +0800

    Add files via upload

--- FeatureMapGeneration.py
+++ FeatureMapGeneration.py
@@ -1,4 +1,5 @@
-from SKMRradiomics import featureextractor
+from SKMRradiomics import featureextractor as skmrradiomics
+from radiomics import featureextractor as originalradiomcis
 # import os
 import csv
 import copy
@@ -8,18 +9,22 @@ import glob
 import six
 
 
-def GetFeatureMap(params_path, store_path, image_path, roi_path):
-    extractor = featureextractor.RadiomicsFeaturesExtractor(params_path, store_path)
-    result = extractor.execute(image_path, roi_path, voxelBased=False)
-    print(result)
-    # for key, val in six.iteritems(result):
-    #     if isinstance(val, sitk.Image):
-    #         shape = (sitk.GetArrayFromImage(val)).shape
-    #         print('feature_map shape is ', shape)
-    #         # Feature map
-    #         sitk.WriteImage(val, store_path + '\\' + key + '.nrrd', True)
-    #     else:  # Diagnostic information
-    #         print("\t%s: %s" % (key, val))
+def GetFeatureMap(params_path, store_path, image_path, roi_path,voxelBasedSet=True):
+    if not voxelBasedSet:
+        extractor = skmrradiomics.RadiomicsFeaturesExtractor(params_path, store_path)
+        result = extractor.execute(image_path, roi_path, voxelBased=voxelBasedSet)
+    if voxelBasedSet:
+        extractor = originalradiomcis.RadiomicsFeaturesExtractor(params_path, store_path)
+        result = extractor.execute(image_path, roi_path, voxelBased=voxelBasedSet)
+        for key, val in six.iteritems(result):
+            if isinstance(val, sitk.Image):
+                shape = (sitk.GetArrayFromImage(val)).shape
+                print('feature_map shape is ', shape)
+                # Feature map
+                sitk.WriteImage(val, store_path + '\\' + key + '.nrrd', True)
+            else:  # Diagnostic information
+                print("\t%s: %s" % (key, val))
+
 
 if __name__ == "__main__":
 
