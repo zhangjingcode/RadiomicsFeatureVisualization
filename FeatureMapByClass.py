2642d5319aa4ec1548b5481f884ca8f284e82197
zhangjingmri <798582238@qq.com>
2020-03-13 00:29:51 +0800

    Previous Pyradiomics version
    1.change to pyradiomics 0.2.2
    2.add iteration of candidate seeking

--- FeatureMapByClass.py
+++ FeatureMapByClass.py
@@ -12,17 +12,16 @@
 
 
 import os
+import time
 import six
 
 import SimpleITK as sitk
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from radiomics import featureextractor
+from SKMRradiomics import featureextractor
 
 from RadiomicsFeatureVisualization.FeatureMapShow import FeatureMapVisualizition
-
-
 
 class FeatureMapper:
     """
@@ -36,12 +35,14 @@ class FeatureMapper:
         self.selected_feature_list = []
         self.store_path = ''
         self.kernelRadius = ''
+        self.sub_img_array = np.array([])
+        self.sub_roi_array = np.array([])
 
     def load(self, feature_csv_path, selected_feature_list):
         self.feature_pd = pd.read_csv(feature_csv_path, index_col=0)
         self.selected_feature_list = selected_feature_list
 
-    def seek_candidate_case(self, feature_name):
+    def seek_single_candidate_case(self, feature_name):
         sub_feature_pd = self.feature_pd[['label', feature_name]].copy()
         sub_feature_pd.sort_values(by=feature_name, inplace=True)
         sorted_index_list = sub_feature_pd.axes[0]
@@ -50,6 +51,22 @@ class FeatureMapper:
         max_info = max_case + '(' + str(int(sub_feature_pd.at[max_case, 'label'])) + ')'
         min_info = min_case + '(' + str(int(sub_feature_pd.at[min_case, 'label'])) + ')'
         print('{} value maximum case : {}, minimum case : {}'.format(feature_name,max_info, min_info))
+
+    def seek_candidate_case(self, feature_csv_path, selected_feature_list):
+        """
+            seek a candidate image by feature value from feature csv， print the candidate case.
+
+        Parameters
+        ----------
+        feature_csv_path : str, radiomics feature csv;
+        selected_feature_list : list, selected feature name list;
+
+
+        """
+        self.load(feature_csv_path, selected_feature_list)
+
+        for sub_feature in selected_feature_list:
+            self.seek_single_candidate_case(sub_feature)
 
     @staticmethod
     def decode_feature_name(feature_name):
@@ -75,7 +92,7 @@ class FeatureMapper:
         y_index = np.where(np.sum(roi_array[max_roi_slice_index], axis=1) > 0)[0]
         y_range = [min(y_index) - self.kernelRadius, max(y_index) + self.kernelRadius + 1]
 
-        cropped_roi_array = roi_array[z_range[0]:z_range[1], y_range[0]:y_range[1],x_range[0]:x_range[1]]
+        cropped_roi_array = roi_array[z_range[0]:z_range[1]]
         cropped_roi = sitk.GetImageFromArray(cropped_roi_array)
         cropped_roi.SetDirection(roi.GetDirection())
         cropped_roi.SetOrigin(roi.GetOrigin())
@@ -83,7 +100,7 @@ class FeatureMapper:
 
         img = sitk.ReadImage(img_path)
         img_array = sitk.GetArrayFromImage(img)
-        cropped_img_array = img_array[z_range[0]:z_range[1], y_range[0]:y_range[1],x_range[0]:x_range[1]]
+        cropped_img_array = img_array[z_range[0]:z_range[1]]
         cropped_img = sitk.GetImageFromArray(cropped_img_array)
         cropped_img.SetDirection(img.GetDirection())
         cropped_img.SetOrigin(img.GetOrigin())
@@ -93,27 +110,53 @@ class FeatureMapper:
         from MeDIT.Normalize import Normalize01
         Imshow3DArray(Normalize01(np.transpose(cropped_img_array, (1,2,0))),
                       roi=np.transpose(cropped_roi_array, (1,2,0)))
-
+        sitk.WriteImage(cropped_img, os.path.join(self.store_path, 'cropped_img.nii.gz'))
+        sitk.WriteImage(cropped_roi, os.path.join(self.store_path, 'cropped_roi.nii.gz'))
+        self.sub_img_array = np.transpose(cropped_img_array, (1, 2, 0))
+        self.sub_roi_array = np.transpose(cropped_roi_array, (1, 2, 0))
         return cropped_img, cropped_roi
 
     def generate_feature_map(self, img_path, roi_path, kernelRadius, feature_name, store_path):
+        """
+            Generate specific feature map based on kernel Radius.
+
+        Parameters
+        ----------
+        img_path: str, candidate image path;
+        roi_path: str, candidate ROI path;
+        kernelRadius: integer, specifies the size of the kernel to use as the radius from the center voxel. \
+                    Therefore the actual size is 2 * kernelRadius + 1. E.g. a value of 1 yields a 3x3x3 kernel, \
+                    a value of 2 5x5x5, etc. In case of 2D extraction, the generated kernel will also be a 2D shape
+                    (square instead of cube).
+        feature_name: str;
+        store_path: str;
+
+        Returns
+        -------
+
+        """
+
+        start_time = time.time()
         self.kernelRadius = kernelRadius
         self.store_path = store_path
         parameter_path = r'D:\hospital\EENT\code\FeatureMap\RadiomicsParams.yaml'
-        setting_dict = {'label': 1, 'interpolator': 'sitkBSpline', 'correctMask': True, 'geometryTolerance': 0.1,
-                        'kernelRadius': self.kernelRadius}
+        setting_dict = {'label': 1, 'interpolator': 'sitkBSpline', 'correctMask': True,
+                        'geometryTolerance': 0.1, 'kernelRadius': self.kernelRadius,
+                        'maskedKernel': True, 'voxelBatch':50}
 
-        #kernelRadius=self.kernelRadius
-        extractor = featureextractor.RadiomicsFeatureExtractor(parameter_path)
-        # extractor.disableAllFeatures()
+        extractor = featureextractor.RadiomicsFeaturesExtractor(**setting_dict)
+        extractor.disableAllFeatures()
 
-        # img_setting, feature_dict = self.decode_feature_name(feature_name)
+        img_setting, feature_dict = self.decode_feature_name(feature_name)
 
-        # extractor.enableFeaturesByName(**img_setting, **feature_dict)
+        extractor.enableFeaturesByName(**img_setting, **feature_dict)
 
         cropped_img, cropped_roi = self.crop_img(roi_path, img_path)
         result = extractor.execute(cropped_img, cropped_roi, voxelBased=True)
+        # without parameters, glcm ,kr=5 ,646s ,cropped img, map shape (5, 132, 128)
+        # without parameters, glcm ,kr=1 ,386s ,cropped img, map shape (3, 122, 128)
 
+        # without parameters, glcm ,kr=1 ,566s ,without cropped img, map shape (5, 132, 128)
         # result = extractor.execute(img_path, roi_path, voxelBased=True)
         for key, val in six.iteritems(result):
             if isinstance(val, sitk.Image):
@@ -121,25 +164,32 @@ class FeatureMapper:
                 print('feature_map shape is ', shape)
                 # Feature map
                 sitk.WriteImage(val, store_path + '\\' + key + '.nrrd', True)
+                print(time.time()-start_time)
 
-    def show_feature_map(self, image_path, roi_path, feature_map_path):
+    def show_feature_map(self, img_path, roi_path, feature_map_path):
+        from MeDIT.SaveAndLoad import LoadNiiData
+        _, _, feature_map_array = LoadNiiData(feature_map_path)
         featuremapvisualization = FeatureMapVisualizition()
-        featuremapvisualization.LoadData(image_path, roi_path, feature_map_path)
+        featuremapvisualization.LoadData(r'D:\hospital\EENT\code\FeatureMap\N45\data1.nii',
+                                         r'D:\hospital\EENT\code\FeatureMap\N45\ROI.nii', feature_map_path)
 
         store_figure_path = self.store_path + '\\' + (os.path.split(feature_map_path)[-1]).split('.')[0]
         # hsv/jet/gist_rainbow
-        # featuremapvisualization.Show(color_map='seismic')
-        featuremapvisualization.Show(color_map='rainbow', store_path=store_figure_path)
+        featuremapvisualization.Show(color_map='rainbow',store_path=store_figure_path)
 
 
-feature_mapper = FeatureMapper()
 #
-# feature_mapper.load(r'D:\hospital\EENT\New_test\SeparateByDate\3D_3.0\features3D.csv',
+feature_mapper = FeatureMapper()
+# #
+# feature_mapper.seek_candidate_case(r'D:\hospital\EENT\New_test\SeparateByDate\3D_3.0\features3D.csv',
 #                     ['T2_original_glcm_DifferenceEntropy', 'T2_original_glrlm_LongRunEmphasis',
 #                      'T2_original_glszm_HighGrayLevelZoneEmphasis'])
+img_path = r'D:\hospital\EENT\code\FeatureMap\N45\data1.nii'
+roi_path = r'D:\hospital\EENT\code\FeatureMap\N45\ROI.nii'
 
-# feature_mapper.seek_candidate_case('T2_original_glcm_DifferenceEntropy')
-feature_mapper.generate_feature_map(r'D:\hospital\EENT\code\FeatureMap\N45\data1.nii',
-                                    r'D:\hospital\EENT\code\FeatureMap\N45\ROI.nii', 1,
-                                    'original_glrlm_RunVariance',
+feature_mapper.generate_feature_map(img_path, roi_path, 1,
+                                    'original_glcm_DifferenceEntropy',
                                     r'D:\hospital\EENT\code\FeatureMap\feature_map')
+
+feature_map_path =r'D:\hospital\EENT\code\FeatureMap\feature_map\original_glcm_DifferenceEntropy.nrrd'
+feature_mapper.show_feature_map(img_path, roi_path, feature_map_path)
